ad92e05c6edabbdc87fff4178b25055d24d7d5c7
Mikhail Kozachenko <kozachenko.mikhail@softkitit.com>
2022-11-26 10:20:18 +0200

    add

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,3 +1,4 @@
+import Profile from "./Profile"
 export const App = () => {
   return (
     <div
@@ -10,7 +11,7 @@ export const App = () => {
         color: '#010101'
       }}
     >
-      React homework template
+      <Profile title="Hello Word!!!" />
     </div>
   );
 };
